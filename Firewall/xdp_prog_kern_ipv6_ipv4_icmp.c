9ab26b1365e0812212fb1e17f91e11c4e3a98962
Asghar <s.aazaidi2001@gmail.com>
2026-01-29 08:01:24 +0500

    added the functionality of parsing udp and tcp as well, need to work on flow more

--- Firewall/xdp_prog_kern_ipv6_ipv4_icmp.c
+++ Firewall/xdp_prog_kern_ipv6_ipv4_icmp.c
@@ -17,14 +17,32 @@
 #include "../xdp-tutorial/common/xdp_stats_kern_user.h"
 #include "../xdp-tutorial/common/xdp_stats_kern.h"
 
-#include <linux/tcp.h>
-#include <linux/udp.h>
-
-
 /* Header cursor to keep track of current parsing position */
 struct hdr_cursor {
 	void *pos;
 };
+
+//the VLAN tag header is not exported by any of the IP header files.
+// struct vlan_hdr {
+// 	__be16	h_vlan_TCI;
+// 	__be16	h_vlan_encapsulated_proto;
+// };
+
+// static __always_inline int proto_is_vlan(__u16 h_proto)
+// {
+// 	return !!(h_proto == bpf_htons(ETH_P_8021Q) ||
+// 		  h_proto == bpf_htons(ETH_P_8021AD));
+// }
+/* Packet parsing helpers.
+ *
+ * Each helper parses a packet header, including doing bounds checking, and
+ * returns the type of its contents if successful, and -1 otherwise.
+ *
+ * For Ethernet and IP headers, the content type is the type of the payload
+ * (h_proto for Ethernet, nexthdr for IPv6), for ICMP it is the ICMP type field.
+ * All return values are in network byte order.
+ */
+
 
 static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
 					void *data_end,
@@ -32,11 +50,50 @@ static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
 {
 	struct ethhdr *eth = nh->pos;
 	int hdrsize = sizeof(*eth);
+
+	//assignment 4 addition
+	// struct vlan_hdr *vlh;
+	// __u16 h_proto;
+	// int i;
+
+	/* Byte-count bounds check; check if current pointer + size of header
+	 * is after data_end.
+	 */
+
+	// This was the one that was given previously, moving the pointer by 1 byte is not enough.
+	// if (nh->pos + 1 > data_end)
+	// 	return -1;
+
 	if (nh->pos + hdrsize > data_end)
 		return -1;
 		
 	nh->pos += hdrsize;
 	*ethhdr = eth;
+
+	//ass4
+	// vlh = nh->pos;
+	// h_proto = eth->h_proto;
+
+	/* Use loop unrolling to avoid the verifier restriction on loops;
+	 * support up to VLAN_MAX_DEPTH layers of VLAN encapsulation.
+	 */
+	// #pragma unroll
+	// for (i = 0; i < VLAN_MAX_DEPTH; i++) {
+	// 	if (!proto_is_vlan(h_proto))
+	// 		break;
+
+	// 	if (vlh + 1 > data_end)
+	// 		break;
+
+	// 	h_proto = vlh->h_vlan_encapsulated_proto;
+	// 	if (vlans) /* collect VLAN ids */
+	// 		vlans->id[i] =
+	// 			(bpf_ntohs(vlh->h_vlan_TCI) & VLAN_VID_MASK);
+
+	// 	vlh++;
+	// }
+
+	// nh->pos = vlh;
 
 	return eth->h_proto; /* network-byte-order */
 }
@@ -116,64 +173,6 @@ static __always_inline int parse_icmphdr(struct hdr_cursor *nh,
 	return icmph->type;
 }
 
-// Ethernet
-//  ├── IPv6
-//  │    ├── ICMPv6
-//  │    ├── TCP
-//  │    └── UDP
-//  └── IPv4
-//       ├── ICMP
-//       ├── TCP
-//       └── UDP
-
-
-//parse_udphdr: parse the udp header and return the length of the udp payload
-static __always_inline int parse_udphdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct udphdr **udphdr)
-{
-	int len;
-	struct udphdr *h = nh->pos;
-
-	if (h + 1 > data_end)
-		return -1;
-
-	nh->pos  = h + 1;
-	*udphdr = h;
-
-	len = bpf_ntohs(h->len) - sizeof(struct udphdr);
-	if (len < 0)
-		return -1;
-
-	return len;
-}
-
-//parse_tcphdr: parse and return the length of the tcp header
-static __always_inline int parse_tcphdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct tcphdr **tcphdr)
-{
-	int len;
-	struct tcphdr *h = nh->pos;
-
-	if (h + 1 > data_end)
-		return -1;
-
-	len = h->doff * 4;
-	/* Sanity check packet field is valid */
-	if(len < sizeof(*h))
-		return -1;
-
-	/* Variable-length TCP header, need to use byte-based arithmetic */
-	if (nh->pos + len > data_end)
-		return -1;
-
-	nh->pos += len;
-	*tcphdr = h;
-
-	return len;
-}
-
 SEC("xdp")
 int  xdp_parser_func(struct xdp_md *ctx)
 {
@@ -183,10 +182,11 @@ int  xdp_parser_func(struct xdp_md *ctx)
 	struct ipv6hdr *ip6h;
 	struct icmp6hdr *icmp6h;
 
-	struct tcphdr *tcph;
-	struct udphdr *udph;
-	//Default action
-	__u32 action = XDP_PASS;
+	/* Default action XDP_PASS, imply everything we couldn't parse, or that
+	 * we don't want to deal with, we just pass up the stack and let the
+	 * kernel deal with it.
+	 */
+	__u32 action = XDP_PASS; /* Default action */
 
 	/* These keep track of the next header type and iterator pointer */
 	struct hdr_cursor nh;
@@ -195,11 +195,23 @@ int  xdp_parser_func(struct xdp_md *ctx)
 	/* Start next header cursor position at data start */
 	nh.pos = data;
 
+	/* Packet parsing in steps: Get each header one at a time, aborting if
+	 * parsing fails. Each helper function does sanity checking (is the
+	 * header type in the packet correct?), and bounds checking.
+	 */
+	// nh_type = parse_ethhdr(&nh, data_end, &eth);
+	// if (nh_type != bpf_htons(ETH_P_IPV6))
+	// 	goto out;
+
+
 	/* ---- Ethernet ---- */
 	nh_type = parse_ethhdr(&nh, data_end, &eth);
 	if (nh_type < 0)
 		goto out;
 
+	// Adding the functionality of ipv4
+	// if (nh_type != bpf_htons(ETH_P_IPV6))
+	// 	goto out;
 
 	/* ---- IPv6 ---- */
 	if (nh_type == bpf_htons(ETH_P_IPV6)){
@@ -207,25 +219,12 @@ int  xdp_parser_func(struct xdp_md *ctx)
 		if (nh_type < 0)
 			goto out;
 
-		// if (nh_type != IPPROTO_ICMPV6)
-		// 	goto out;
+		if (nh_type != IPPROTO_ICMPV6)
+			goto out;
 
 		/* ---- ICMPv6 ---- */
-		if (nh_type == IPPROTO_ICMPV6) {
-			if (parse_icmp6hdr(&nh, data_end, &icmp6h) < 0)
-				goto out;
-		}
-		else if (nh_type == IPPROTO_TCP) {
-			if (parse_tcphdr(&nh, data_end, &tcph) < 0)
-				goto out;
-		}
-		else if (nh_type == IPPROTO_UDP) {
-			if (parse_udphdr(&nh, data_end, &udph) < 0)
-				goto out;
-		}
-		else {
+		if (parse_icmp6hdr(&nh, data_end, &icmp6h) < 0)
 			goto out;
-		}
 		action = XDP_PASS;
 	}
 
@@ -238,27 +237,11 @@ int  xdp_parser_func(struct xdp_md *ctx)
 		if (nh_type < 0)
 			goto out;
 
-		// if (nh_type != IPPROTO_ICMP)
-		// 	goto out;
-
-		if (nh_type == IPPROTO_ICMP) {
-			if (parse_icmphdr(&nh, data_end, &icmph) < 0)
-				goto out;
-		}
-		else if (nh_type == IPPROTO_TCP) {
-			if (parse_tcphdr(&nh, data_end, &tcph) < 0)
-				goto out;
-		}
-		else if (nh_type == IPPROTO_UDP) {
-			if (parse_udphdr(&nh, data_end, &udph) < 0)
-				goto out;
-		}
-		else {
+		if (nh_type != IPPROTO_ICMP)
 			goto out;
-		}
 
-		// if (parse_icmphdr(&nh, data_end, &icmph) < 0)
-		// 	goto out;
+		if (parse_icmphdr(&nh, data_end, &icmph) < 0)
+			goto out;
 
 		/* IPv4 logic here */
 		action = XDP_PASS;
@@ -269,6 +252,23 @@ int  xdp_parser_func(struct xdp_md *ctx)
 		goto out;
 	}
 
+	
+    /* Only acts on echo requests */
+    // if (icmp6h->icmp6_type == ICMPV6_ECHO_REQUEST) { //ensure this is echo request
+	// 	 /* Convert sequence number from network to host order, only if needed if it detects in compilation time */
+    //     __u16 seq = bpf_ntohs(icmp6h->icmp6_sequence); //extract sequence number
+
+    //     if ((seq % 2) == 0)
+    //         action = XDP_DROP;
+    //     else
+    //         action = XDP_PASS;
+
+    //     goto out;
+    // }
+	// action = XDP_DROP;
+
+	// comment this out if you wanna do the decomment above thing
+	// action = XDP_PASS;
 out:
 	return xdp_stats_record_action(ctx, action); /* read via xdp_stats */
 }
